f2f6e6691ff700c8c9a4eeb40b8faa873dbc439d
Hung Pham <hung.pham@dirox.net>
2025-06-18 06:21:20 +0700

    Fix wrong labeling

--- backend/src/actors/tcbs/monitor.rs
+++ backend/src/actors/tcbs/monitor.rs
@@ -3,8 +3,6 @@ use std::sync::Arc;
 
 use super::Order;
 
-#[cfg(not(feature = "python"))]
-#[inline]
 pub fn monitor_order_flow(symbol: &str, order: &Order, order_counter: Arc<IntCounterVec>) {
     order_counter.with_label_values(&[symbol]).inc_by(order.v);
 }
