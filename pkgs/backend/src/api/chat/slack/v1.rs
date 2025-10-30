de070b4ddfc3609735b4ca9b331f937f12b3846e
hungpham10 <136320753+hungpham10@users.noreply.github.com>
2025-10-30 16:02:46 +0700

    Fix issue message from slack will be echoed back to users (#224)
    * Setup FACEBOOK_OUTGOING_SECRET to reply messages to users (#222)
    
    * Improve by checking if the message is from bot or by real employees

--- pkgs/backend/src/api/chat/slack/v1.rs
+++ pkgs/backend/src/api/chat/slack/v1.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
+use log::info;
+
 use actix_web::error::ErrorInternalServerError;
-use actix_web::web::{Data, Json};
+use actix_web::web::{Bytes, Data, Query};
 use actix_web::{Error, HttpResponse, Result};
 
 use serde::{Deserialize, Serialize};
@@ -10,23 +12,27 @@ use crate::api::chat::facebook::send_message;
 use crate::api::chat::ChatHeaders;
 use crate::api::AppState;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 pub struct SlackMessage {
     channel: String,
     text: String,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SlackEvent {
     #[serde(rename = "type")]
     event_type: String,
+
+    #[serde(rename = "bot_id")]
+    bot: Option<String>,
+
     user: Option<String>,
     text: Option<String>,
     channel: Option<String>,
     thread_ts: Option<String>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct SlackEventWrapper {
     token: String,
     challenge: Option<String>,
@@ -35,9 +41,11 @@ pub struct SlackEventWrapper {
 
 pub async fn receive_message(
     appstate: Data<Arc<AppState>>,
-    payload: Json<SlackEventWrapper>,
+    body: Bytes,
     headers: ChatHeaders,
 ) -> Result<HttpResponse> {
+    let payload = serde_json::from_slice::<SlackEventWrapper>(&body)?;
+
     if let Some(challenge) = &payload.challenge {
         return Ok(HttpResponse::Ok().body(challenge.clone()));
     }
@@ -45,27 +53,29 @@ pub async fn receive_message(
     if let Some(entity) = appstate.chat_entity() {
         if let Some(event) = &payload.event {
             if event.event_type == "message" {
-                if let (Some(user), Some(text), Some(thread_ts)) =
-                    (&event.user, &event.text, &event.thread_ts)
-                {
-                    match entity
-                        .get_sender_id_by_thread(headers.tenant_id, thread_ts)
-                        .await
+                if event.bot.is_none() {
+                    if let (Some(user), Some(text), Some(thread_ts)) =
+                        (&event.user, &event.text, &event.thread_ts)
                     {
-                        Ok(Some(sender_id)) => {
-                            send_message(&appstate, &sender_id, &text)
-                                .await
-                                .map_err(|error| do_send_message_falure(error))?
-                            // @TODO: store message to parquet in S3
-                        }
-                        Ok(None) => {
-                            // @TODO: maybe this is command from CSKH
-                        }
-                        Err(error) => {
-                            return Err(ErrorInternalServerError(format!(
-                                "Fail to get sender.id: {}",
-                                error
-                            )));
+                        match entity
+                            .get_sender_id_by_thread(headers.tenant_id, thread_ts)
+                            .await
+                        {
+                            Ok(Some(sender_id)) => {
+                                send_message(&appstate, &sender_id, &text)
+                                    .await
+                                    .map_err(|error| do_send_message_falure(error))?
+                                // @TODO: store message to parquet in S3
+                            }
+                            Ok(None) => {
+                                // @TODO: maybe this is command from CSKH
+                            }
+                            Err(error) => {
+                                return Err(ErrorInternalServerError(format!(
+                                    "Fail to get sender.id: {}",
+                                    error
+                                )));
+                            }
                         }
                     }
                 }
