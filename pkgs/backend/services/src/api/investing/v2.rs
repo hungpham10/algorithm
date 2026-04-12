dc54897a8aed14dbc3ce94fc157ce4ddf8a0ca70
Hung Pham <hung0913208@icloud.com>
2026-04-12 16:15:57 +0700

    Change to correct path to avoid JWT token

--- pkgs/backend/services/src/api/investing/v2.rs
+++ pkgs/backend/services/src/api/investing/v2.rs
@@ -36,11 +36,11 @@ impl Modify for SecurityAddon {
     fn modify(&self, openapi: &mut utoipa::openapi::OpenApi) {
         let components = openapi.components.as_mut().unwrap();
         components.add_security_scheme(
-            "bearer_auth", // Phải khớp với tên trong #[utoipa::path]
+            "bearer_auth",
             SecurityScheme::Http(
                 HttpBuilder::new()
                     .scheme(HttpAuthScheme::Bearer)
-                    .bearer_format("JWT") // Mô tả định dạng là JWT
+                    .bearer_format("JWT")
                     .build(),
             ),
         );
